1cca27fc8f7966be88401b6b09f8b42f30f1e4f2
MurilinhoPs <murilinhorps@gmail.com>
2021-01-13 17:43:09 -0300

    Remove stack from weapons to add a Canvas to paint the lines Need to organize the Canvas on another file

--- lib/app/modules/firstPage/wdigets/equipments/weapons.dart
+++ lib/app/modules/firstPage/wdigets/equipments/weapons.dart
@@ -7,89 +7,81 @@ class Weapons extends StatelessWidget {
   Widget build(BuildContext context) {
     return BoxContainer(
       bottom: myDefaultBorder,
-      child: ConstrainedBox(
-        // height: 210,
-        constraints: BoxConstraints(minHeight: 100),
-        child: Stack(
+      child: CustomPaint(
+        foregroundPainter: LinesPainter(),
+        child: Column(
           children: [
-            Align(
-              alignment: Alignment.topCenter,
-              child: BoxContainer(
-                bottom: myDefaultBorder,
-                child: Container(
-                  decoration: BoxDecoration(
-                    gradient: LinearGradient(
-                      colors: [AppColors.darkBrown, AppColors.lightBrown],
-                      stops: [0.3, 0.95],
-                    ),
-                  ),
-                  height: 52.0,
-                  child: Row(
-                    children: [
-                      Expanded(
-                        flex: 2,
-                        child: Row(
-                          children: [
-                            Container(
-                              alignment: Alignment.centerLeft,
-                              height: 55.0,
-                              width: 100,
-                              padding: EdgeInsets.all(4.0),
-                              child: Text(
-                                'ARMA',
-                                style: TextStyle(
-                                  fontSize: 26,
-                                  color: Colors.white,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
-                            ),
-                            Container(
-                              // margin: const EdgeInsets.all(2.5),
-                              decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(25.0),
-                                color: AppColors.darkGrey,
-                              ),
-                              padding: const EdgeInsets.all(7.0),
-                              child: Icon(
-                                Icons.add_moderator,
-                                size: 22.0,
-                                color: AppColors.lightTextOrange,
-                              ),
-                            )
-                          ],
-                        ),
-                      ),
-                      status(title: 'Bônus'),
-                      status(title: 'Dano'),
-                      status(title: 'Alcance', border: BorderSide.none),
-                    ],
+            BoxContainer(
+              bottom: myDefaultBorder,
+              child: Container(
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    colors: [AppColors.darkBrown, AppColors.lightBrown],
+                    stops: [0.3, 0.95],
                   ),
                 ),
-              ),
-            ),
-            Positioned(
-              top: 54,
-              right: 0,
-              left: 0,
-              child: Container(
-                padding: const EdgeInsets.all(10.0),
-                // height: 156,
-                color: AppColors.lightOrange,
-                child: Column(
-                  mainAxisSize: MainAxisSize.max,
-                  crossAxisAlignment: CrossAxisAlignment.start,
+                height: 52.0,
+                child: Row(
                   children: [
-                    Text('Escrever as armas'),
-                    Text('Escrever as armas'),
-                    Text('Escrever as armas'),
+                    Expanded(
+                      flex: 2,
+                      child: Row(
+                        children: [
+                          Container(
+                            alignment: Alignment.centerLeft,
+                            height: 55.0,
+                            width: 100,
+                            padding: EdgeInsets.all(4.0),
+                            child: Text(
+                              'ARMA',
+                              style: TextStyle(
+                                fontSize: 26,
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                          Container(
+                            // margin: const EdgeInsets.all(2.5),
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(25.0),
+                              color: AppColors.darkGrey,
+                            ),
+                            padding: const EdgeInsets.all(7.0),
+                            child: Icon(
+                              Icons.add_moderator,
+                              size: 22.0,
+                              color: AppColors.lightTextOrange,
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                    status(title: 'Bônus'),
+                    status(title: 'Dano'),
+                    status(title: 'Alcance', border: BorderSide.none),
                   ],
                 ),
               ),
             ),
-            positionedBorder(165, context),
-            positionedBorder(253.3, context),
-            positionedBorder(343.6, context),
+            Container(
+              padding: const EdgeInsets.all(10.0),
+              // height: 156,
+              width: double.infinity,
+              color: AppColors.lightOrange,
+              child: Column(
+                mainAxisSize: MainAxisSize.max,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text('Escrever as armas'),
+                  Text('Escrever as armas'),
+                  Text('Escrever as armas'),
+                  Text('Escrever as armas'),
+                  Text('Escrever as armas'),
+                  Text('Escrever as armas'),
+                ],
+              ),
+            ),
           ],
         ),
       ),
@@ -100,7 +92,7 @@ class Weapons extends StatelessWidget {
     return Positioned(
       top: 0,
       bottom: 0,
-      left: left * (MediaQuery.of(context).size.width / 430),
+      left: left * (MediaQuery.of(context).size.width / 440),
       child: BoxContainer(
         right: myDefaultBorder,
         child: null,
@@ -113,7 +105,7 @@ class Weapons extends StatelessWidget {
       flex: flex ?? 1,
       child: Container(
         height: 55.0,
-        alignment: Alignment(0, 0.5),
+        alignment: Alignment(-0.5, 0.5),
         padding: const EdgeInsets.symmetric(horizontal: 5.0),
         child: FittedBox(
           fit: BoxFit.fitWidth,
@@ -128,5 +120,41 @@ class Weapons extends StatelessWidget {
         ),
       ),
     );
+  }
+}
+
+class LinesPainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    final offset1 = 170 * (size.width / 440);
+    final offset2 = 253.3 * (size.width / 440);
+    final offset3 = 340.6 * (size.width / 440);
+
+    final paintWhite = Paint()..color = Colors.transparent;
+    var rrectWhite = RRect.fromLTRBR(
+      0,
+      0,
+      size.width,
+      size.height,
+      Radius.zero,
+    );
+
+    canvas.drawRRect(rrectWhite, paintWhite);
+
+    final paintverticalLines = Paint()..strokeWidth = 2.0;
+
+    canvas.drawLine(
+        Offset(offset1, 0), Offset(offset1, size.height), paintverticalLines);
+
+    canvas.drawLine(
+        Offset(offset2, 0), Offset(offset2, size.height), paintverticalLines);
+
+    canvas.drawLine(
+        Offset(offset3, 0), Offset(offset3, size.height), paintverticalLines);
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    return false;
   }
 }
