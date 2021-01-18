e1fbd5622198638d900a8dfae5d0b34a6811db84
murilinhoPs <murilinhops@hotmail.com>
2021-01-18 09:14:16 -0300

    Change folder name from wdigets to widgets

--- lib/app/modules/firstPage/first_page.dart
+++ lib/app/modules/firstPage/first_page.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:pdf_mutant/app/modules/firstPage/wdigets/atributes.dart';
-import 'package:pdf_mutant/app/modules/firstPage/wdigets/conditions/conditions.dart';
-import 'package:pdf_mutant/app/modules/firstPage/wdigets/equipments/all_equips.dart';
-import 'package:pdf_mutant/app/modules/firstPage/wdigets/name_basics.dart';
-import 'package:pdf_mutant/app/modules/firstPage/wdigets/points.dart';
-import 'package:pdf_mutant/app/modules/firstPage/wdigets/skills.dart';
+import 'package:pdf_mutant/app/modules/firstPage/widgets/atributes.dart';
+import 'package:pdf_mutant/app/modules/firstPage/widgets/conditions/conditions.dart';
+import 'package:pdf_mutant/app/modules/firstPage/widgets/equipments/all_equips.dart';
+import 'package:pdf_mutant/app/modules/firstPage/widgets/name_basics.dart';
+import 'package:pdf_mutant/app/modules/firstPage/widgets/points.dart';
+import 'package:pdf_mutant/app/modules/firstPage/widgets/skills.dart';
 
 class FirstPage extends StatelessWidget {
   @override
