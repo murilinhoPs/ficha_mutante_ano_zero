ef480228ec3e717682586e182d4e86b87db7bacf
murilinhops <murilinhorps@gmail.com>
2021-03-26 14:55:33 -0300

    Instantiate LocalStorageWrapper.initLocalStorage before run app

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:pdf_mutant/src/app_module.dart';
+import 'package:pdf_mutant/src/global/services/local_storage/local_storage_impl.dart';
+import 'package:pdf_mutant/src/global/services/local_storage/local_storage_wrapper.dart';
 
-void main() => runApp(AppModule());
+void main() {
+  LocalStorageWrapper.initLocalStorage(LocalStorageImpl());
+
+  runApp(AppModule());
+}
