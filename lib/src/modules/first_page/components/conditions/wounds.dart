b92a77a0061f9de1b0c7a2145737057ada373770
murilinhops <murilinhorps@gmail.com>
2021-10-16 15:11:21 -0300

    Change Wounds to extends from BlankNotebookWidget

--- lib/src/modules/first_page/components/conditions/wounds.dart
+++ lib/src/modules/first_page/components/conditions/wounds.dart
@@ -1,36 +1,10 @@
-import 'package:flutter/material.dart';
-import 'package:ficha_mutante_ano_zero/src/global/colors.dart';
-import 'package:ficha_mutante_ano_zero/src/widgets/text_inputs/resizible_input.dart';
+import 'package:ficha_mutante_ano_zero/src/widgets/notebook/blank_notebook_widget.dart';
 
-class Wounds extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      color: AppColors.lightOrange,
-      padding: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 8.0),
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Text(
-                'Ferimentos Graves:',
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 14.0,
-                ),
-              ),
-            ],
-          ),
-          ResizableInput(
-            fontSize: 14,
-            hintText: 'Escreva seus ferimentos',
-            keySharedPrefs: 'wounds',
-          )
-        ], // one ok rock
-      ),
-    );
-  }
+class Wounds extends BlankNotebookWidget {
+  Wounds()
+      : super(
+          notebookTitle: 'Ferimentos Graves',
+          hintText: 'Escreva seus ferimentos...',
+          keyToSave: 'wounds',
+        );
 }
